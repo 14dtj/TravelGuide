2becb58220d9ea61aecc133acd5e06ce1becf853
tjDu <457391179@qq.com>
2018-11-24 10:55:48 +0800

    try webview using js api

--- app/src/main/java/cn/edu/sjtu/travelguide/MainActivity.java
+++ app/src/main/java/cn/edu/sjtu/travelguide/MainActivity.java
@@ -1,150 +1,94 @@
 package cn.edu.sjtu.travelguide;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Window;
+import android.webkit.GeolocationPermissions;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import com.baidu.location.BDLocation;
-import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapStatus;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
-import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.model.LatLng;
 
-public class MainActivity extends Activity implements SensorEventListener{
-
-    private MapView mMapView = null;
-    private BaiduMap mBaiduMap;
-
-    LocationClient mLocClient;
-    public MyLocationListenner myListener = new MyLocationListenner();
-    private LocationMode mCurrentMode;
-    //BitmapDescriptor mCurrentMarker;
-//    private static final int accuracyCircleFillColor = 0xAAFFFF88;
-//    private static final int accuracyCircleStrokeColor = 0xAA00FF00;
-    private SensorManager mSensorManager;
-    private Double lastX = 0.0;
-    private int mCurrentDirection = 0;
-    private double mCurrentLat = 0.0;
-    private double mCuurentLon = 0.0;
-    private float mCurrentAccuracy;
-
-    private MyLocationData locData;
-    boolean isFirstLoc = true; // 是否首次定位
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocationClient mLocationClient = new LocationClient(getApplicationContext());
+        WebView webView = this.findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        String dir = this.getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
+        webSettings.setGeolocationDatabasePath(dir);
+        webView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+        });
 
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mCurrentMode = LocationMode.COMPASS;
-
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.bmapView);
-        mBaiduMap = mMapView.getMap();
-        mBaiduMap.setMyLocationEnabled(true);
-        mLocClient = new LocationClient(this);
-        mLocClient.registerLocationListener(myListener);
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true);
-        option.setCoorType("bd09ll");
-        option.setScanSpan(1000);
-        mLocClient.setLocOption(option);
-        mLocClient.start();
-        mLocClient.requestLocation();
+        webView.setWebChromeClient(new WebChromeClient() {
+            // 处理javascript中的alert
+            public boolean onJsAlert(WebView view, String url, String message,
+                                     final JsResult result) {
+                return true;
+            }
+            // 处理javascript中的confirm
+            public boolean onJsConfirm(WebView view, String url,
+                                       String message, final JsResult result) {
+                return true;
+            }
+            // 处理定位权限请求
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin,
+                                                           GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+            }
+            @Override
+            // 设置网页加载的进度条
+            public void onProgressChanged(WebView view, int newProgress) {
+//                TestJSLocation.this.getWindow().setFeatureInt(
+//                        Window.FEATURE_PROGRESS, newProgress * 100);
+                super.onProgressChanged(view, newProgress);
+            }
+            // 设置应用程序的标题title
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
+        });
+        webView.loadUrl("file:///android_asset/test.html");
+        mLocationClient.start();
+        mLocationClient.enableAssistantLocation(webView);
     }
 
     @Override
     protected void onDestroy() {
-        //退出时销毁定位
-        mLocClient.stop();
-        // 关闭定位图层
-        mBaiduMap.setMyLocationEnabled(false);
         super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-        mMapView = null;
     }
+
     @Override
     protected void onResume() {
         super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-        //为系统的方向传感器注册监听器
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_UI);
     }
+
     @Override
     protected void onPause() {
         super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
     }
 
     @Override
     protected void onStop() {
-        //取消注册传感器监听
-        mSensorManager.unregisterListener(this);
         super.onStop();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-//        double x = event.values[SensorManager.DATA_X];
-//        if(Math.abs(x-lastX) > 1.0){
-//            mCurrentDirection = (int) x;
-//            locData = new MyLocationData.Builder()
-//                    .accuracy(mCurrentAccuracy)
-//                    .direction(mCurrentDirection).latitude(mCurrentLat)
-//                    .longitude(mCuurentLon).build();
-//            mBaiduMap.setMyLocationData(locData);
-//        }
-//        lastX = x;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    public class MyLocationListenner implements BDLocationListener{
-
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            if(location == null || mMapView == null){
-                System.out.print("-----------got here");
-                return;
-            }
-            mCurrentLat = location.getLatitude();
-            mCuurentLon = location.getLongitude();
-            mCurrentAccuracy = location.getRadius();
-            locData = new MyLocationData.Builder()
-                    .accuracy(mCurrentAccuracy)
-                    .direction(mCurrentDirection).latitude(mCurrentLat)
-                    .longitude(mCuurentLon).build();
-            mBaiduMap.setMyLocationData(locData);
-            //System.out.print("latitude----:"+location.getLatitude() + "longitude----:"+location.getLongitude());
-            if (isFirstLoc) {
-                isFirstLoc = false;
-                LatLng ll = new LatLng(location.getLatitude(),
-                        location.getLongitude());
-                MapStatus.Builder builder = new MapStatus.Builder();
-                builder.target(ll).zoom(18.0f);
-                //System.out.print("latitude:"+ll.latitude + "longitude:"+ll.longitude);
-                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
-            }
-        }
     }
 }
