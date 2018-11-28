21b03e152c475e0eba23bb451717c2b1bee46356
tjDu <457391179@qq.com>
2018-11-28 16:44:34 +0800

    use qmui

--- app/src/main/java/cn/edu/sjtu/travelguide/MainActivity.java
+++ app/src/main/java/cn/edu/sjtu/travelguide/MainActivity.java
@@ -1,150 +1,29 @@
 package cn.edu.sjtu.travelguide;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.baidu.location.BDLocation;
-import com.baidu.location.BDLocationListener;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapStatus;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
-import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.model.LatLng;
+import com.qmuiteam.qmui.arch.QMUIFragmentActivity;
 
-public class MainActivity extends Activity implements SensorEventListener{
+import cn.edu.sjtu.travelguide.fragment.BaseFragment;
+import cn.edu.sjtu.travelguide.fragment.HomeFragment;
 
-    private MapView mMapView = null;
-    private BaiduMap mBaiduMap;
+public class MainActivity extends QMUIFragmentActivity {
 
-    LocationClient mLocClient;
-    public MyLocationListenner myListener = new MyLocationListenner();
-    private LocationMode mCurrentMode;
-    //BitmapDescriptor mCurrentMarker;
-//    private static final int accuracyCircleFillColor = 0xAAFFFF88;
-//    private static final int accuracyCircleStrokeColor = 0xAA00FF00;
-    private SensorManager mSensorManager;
-    private Double lastX = 0.0;
-    private int mCurrentDirection = 0;
-    private double mCurrentLat = 0.0;
-    private double mCuurentLon = 0.0;
-    private float mCurrentAccuracy;
-
-    private MyLocationData locData;
-    boolean isFirstLoc = true; // 是否首次定位
+    @Override
+    protected int getContextViewId() {
+        return R.id.qmuidemo;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        BaseFragment fragment = new HomeFragment();
 
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        mCurrentMode = LocationMode.COMPASS;
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(getContextViewId(), fragment, fragment.getClass().getSimpleName())
+                .addToBackStack(fragment.getClass().getSimpleName())
+                .commit();
 
-        //获取地图控件引用
-        mMapView = (MapView) findViewById(R.id.bmapView);
-        mBaiduMap = mMapView.getMap();
-        mBaiduMap.setMyLocationEnabled(true);
-        mLocClient = new LocationClient(this);
-        mLocClient.registerLocationListener(myListener);
-        LocationClientOption option = new LocationClientOption();
-        option.setOpenGps(true);
-        option.setCoorType("bd09ll");
-        option.setScanSpan(1000);
-        mLocClient.setLocOption(option);
-        mLocClient.start();
-        mLocClient.requestLocation();
-    }
-
-    @Override
-    protected void onDestroy() {
-        //退出时销毁定位
-        mLocClient.stop();
-        // 关闭定位图层
-        mBaiduMap.setMyLocationEnabled(false);
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
-        mMapView.onDestroy();
-        mMapView = null;
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
-        mMapView.onResume();
-        //为系统的方向传感器注册监听器
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_UI);
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
-        mMapView.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        //取消注册传感器监听
-        mSensorManager.unregisterListener(this);
-        super.onStop();
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-//        double x = event.values[SensorManager.DATA_X];
-//        if(Math.abs(x-lastX) > 1.0){
-//            mCurrentDirection = (int) x;
-//            locData = new MyLocationData.Builder()
-//                    .accuracy(mCurrentAccuracy)
-//                    .direction(mCurrentDirection).latitude(mCurrentLat)
-//                    .longitude(mCuurentLon).build();
-//            mBaiduMap.setMyLocationData(locData);
-//        }
-//        lastX = x;
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    public class MyLocationListenner implements BDLocationListener{
-
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            if(location == null || mMapView == null){
-                System.out.print("-----------got here");
-                return;
-            }
-            mCurrentLat = location.getLatitude();
-            mCuurentLon = location.getLongitude();
-            mCurrentAccuracy = location.getRadius();
-            locData = new MyLocationData.Builder()
-                    .accuracy(mCurrentAccuracy)
-                    .direction(mCurrentDirection).latitude(mCurrentLat)
-                    .longitude(mCuurentLon).build();
-            mBaiduMap.setMyLocationData(locData);
-            //System.out.print("latitude----:"+location.getLatitude() + "longitude----:"+location.getLongitude());
-            if (isFirstLoc) {
-                isFirstLoc = false;
-                LatLng ll = new LatLng(location.getLatitude(),
-                        location.getLongitude());
-                MapStatus.Builder builder = new MapStatus.Builder();
-                builder.target(ll).zoom(18.0f);
-                //System.out.print("latitude:"+ll.latitude + "longitude:"+ll.longitude);
-                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
-            }
-        }
     }
 }
---
+++ app/src/main/java/cn/edu/sjtu/travelguide/fragment/MapFragment.java
@@ -0,0 +1,153 @@
+package cn.edu.sjtu.travelguide.fragment;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.constraint.ConstraintLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.edu.sjtu.travelguide.R;
+
+import static android.content.Context.SENSOR_SERVICE;
+
+public class MapFragment extends BaseFragment implements SensorEventListener {
+    @BindView(R.id.bmapView)
+    MapView mMapView;
+    private BaiduMap mBaiduMap;
+
+    LocationClient mLocClient;
+    public MyLocationListenner myListener = new MyLocationListenner();
+    private LocationMode mCurrentMode;
+    //BitmapDescriptor mCurrentMarker;
+//    private static final int accuracyCircleFillColor = 0xAAFFFF88;
+//    private static final int accuracyCircleStrokeColor = 0xAA00FF00;
+    private SensorManager mSensorManager;
+    private Double lastX = 0.0;
+    private int mCurrentDirection = 0;
+    private double mCurrentLat = 0.0;
+    private double mCuurentLon = 0.0;
+    private float mCurrentAccuracy;
+
+    private MyLocationData locData;
+    boolean isFirstLoc = true; // 是否首次定位
+
+    @Override
+    protected View onCreateView() {
+        ConstraintLayout layout = (ConstraintLayout) LayoutInflater.from(getActivity()).inflate(R.layout.fragment_map, null);
+        ButterKnife.bind(this, layout);
+        mSensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        mCurrentMode = LocationMode.COMPASS;
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+        mLocClient = new LocationClient(this.getContext());
+        mLocClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+        mLocClient.requestLocation();
+        return layout;
+    }
+
+    @Override
+    public void onDestroy() {
+        //退出时销毁定位
+        mLocClient.stop();
+        // 关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+        mMapView = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+        //为系统的方向传感器注册监听器
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        //取消注册传感器监听
+        mSensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        double x = event.values[SensorManager.DATA_X];
+        if (Math.abs(x - lastX) > 1.0) {
+            mCurrentDirection = (int) x;
+            locData = new MyLocationData.Builder()
+                    .accuracy(mCurrentAccuracy)
+                    .direction(mCurrentDirection).latitude(mCurrentLat)
+                    .longitude(mCuurentLon).build();
+            mBaiduMap.setMyLocationData(locData);
+        }
+        lastX = x;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public class MyLocationListenner implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null || mMapView == null) {
+                System.out.print("-----------got here");
+                return;
+            }
+            mCurrentLat = location.getLatitude();
+            mCuurentLon = location.getLongitude();
+            mCurrentAccuracy = location.getRadius();
+            locData = new MyLocationData.Builder()
+                    .accuracy(mCurrentAccuracy)
+                    .direction(mCurrentDirection).latitude(mCurrentLat)
+                    .longitude(mCuurentLon).build();
+            mBaiduMap.setMyLocationData(locData);
+            //System.out.print("latitude----:"+location.getLatitude() + "longitude----:"+location.getLongitude());
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                //System.out.print("latitude:"+ll.latitude + "longitude:"+ll.longitude);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+    }
+}
