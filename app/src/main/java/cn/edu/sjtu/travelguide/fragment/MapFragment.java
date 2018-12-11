f7c0166836a3be9d79879d129f625e8350d84fd6
wanglei <1016412963@qq.com>
2018-12-11 21:31:29 +0800

    fix

--- app/src/main/java/cn/edu/sjtu/travelguide/fragment/MapFragment.java
+++ app/src/main/java/cn/edu/sjtu/travelguide/fragment/MapFragment.java
@@ -41,6 +41,7 @@ import butterknife.ButterKnife;
 import cn.edu.sjtu.travelguide.R;
 import cn.edu.sjtu.travelguide.RoutePlanActivity;
 import cn.edu.sjtu.travelguide.SearchActivity;
+import cn.edu.sjtu.travelguide.SlideVerticalActivity;
 
 import static android.content.Context.SENSOR_SERVICE;
 
@@ -171,7 +172,7 @@ public class MapFragment extends BaseFragment implements SensorEventListener, On
                     case R.id.fab:
                         String departureLocation = destination.getText().toString();//出发地
                         String destinationLocation = departure.getText().toString();//目的地
-                        Intent intent = new Intent(getActivity(), RoutePlanActivity.class);
+                        Intent intent = new Intent(getActivity(), SlideVerticalActivity.class);
                         intent.putExtra("departureLocation", departureLocation);
                         intent.putExtra("destinationLocation", destinationLocation);
                         startActivity(intent);
